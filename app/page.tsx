463b5530ba615e941812815a9edbe15b95706565
nnma <nnma06112003@gmail.com>
2026-03-14 00:04:13 +0700

    init porfolio project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="max-w-6xl mx-auto px-4 py- 8">
+      <h1 className="text-5xl font-bold">Hi, I'm Minh Anh</h1>
+      <p className="text-xl text-gray-600">Developer</p>
+      <p className="text-lg">I build things</p>
+    </main>
+  );
+}
